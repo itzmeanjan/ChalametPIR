7bf25394ce598224a2befeaab0f555e0e78d48ef
Anjan Roy <hello@itzmeanjan.in>
2025-01-09 17:24:18 +0400

    Generate matrix of dimension `m x n`, given a 32 -bytes seed
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,9 @@
+use sha3::{
+    digest::{ExtendableOutput, Update, XofReader},
+    Shake128,
+};
+use std::cmp::min;
+
 pub struct Matrix {
     rows: usize,
     cols: usize,
@@ -15,5 +21,45 @@ impl Matrix {
                 elems: vec![0; rows * cols],
             })
         }
+    }
+
+    pub fn generate_from_seed(rows: usize, cols: usize, seed: &[u8; 32]) -> Option<Matrix> {
+        let mut hasher = Shake128::default();
+        hasher.update(seed);
+
+        let mut reader = hasher.finalize_xof();
+
+        let mut buffer = [0u8; 168];
+        reader.read(&mut buffer);
+
+        let mut mat = Matrix::new(rows, cols)?;
+        let num_elems = mat.rows * mat.cols;
+
+        let mut cur_elem_idx = 0;
+        let mut buf_offset = 0;
+
+        while cur_elem_idx < num_elems {
+            let fillable_num_elems_from_buf = (buffer.len() - buf_offset) / 4;
+            if fillable_num_elems_from_buf == 0 {
+                reader.read(&mut buffer);
+                buf_offset = 0;
+            }
+
+            let required_num_elems = num_elems - cur_elem_idx;
+            let to_be_filled_num_elems = min(fillable_num_elems_from_buf, required_num_elems);
+
+            let mut local_idx = cur_elem_idx;
+            while local_idx < (cur_elem_idx + to_be_filled_num_elems) {
+                mat.elems[local_idx] =
+                    u32::from_le_bytes(buffer[buf_offset..(buf_offset + 4)].try_into().unwrap());
+
+                local_idx += 1;
+                buf_offset += std::mem::size_of::<u32>();
+            }
+
+            cur_elem_idx += to_be_filled_num_elems;
+        }
+
+        Some(mat)
     }
 }
