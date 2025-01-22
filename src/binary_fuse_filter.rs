2ffcb487cddb3097d31e3958d697ab2a1f5dbf3d
Anjan Roy <hello@itzmeanjan.in>
2025-01-22 21:58:01 +0400

    Support calling filter constructor with ARITY of {3,4}
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/binary_fuse_filter.rs
+++ src/binary_fuse_filter.rs
@@ -23,6 +23,217 @@ impl BinaryFuseFilter {
     ) -> Option<(BinaryFuseFilter, Vec<u64>, Vec<u8>, HashMap<u64, &'a [u8]>)> {
         const { assert!(ARITY == 3 || ARITY == 4) }
 
+        match ARITY {
+            3 => Self::construct_3_wise_xor_filter(db, mat_elem_bit_len, max_attempt_count),
+            4 => Self::construct_4_wise_xor_filter(db, mat_elem_bit_len, max_attempt_count),
+            _ => panic!("Unsupported arity requested !"),
+        }
+    }
+
+    pub fn construct_3_wise_xor_filter<'a>(
+        db: &HashMap<&'a [u8], &[u8]>,
+        mat_elem_bit_len: usize,
+        max_attempt_count: usize,
+    ) -> Option<(BinaryFuseFilter, Vec<u64>, Vec<u8>, HashMap<u64, &'a [u8]>)> {
+        const ARITY: u32 = 3;
+
+        let db_size = db.len();
+        if db_size == 0 {
+            return None;
+        }
+
+        let segment_length = segment_length::<ARITY>(db_size as u32).min(1u32 << 18);
+
+        let size_factor = size_factor::<ARITY>(db_size as u32);
+        let capacity = if db_size > 1 { ((db_size as f64) * size_factor).round() as u32 } else { 0 };
+
+        let init_segment_count = (capacity + segment_length - 1) / segment_length;
+        let (num_fingerprints, segment_count) = {
+            let array_len = init_segment_count * segment_length;
+            let segment_count: u32 = {
+                let proposed = (array_len + segment_length - 1) / segment_length;
+                if proposed < ARITY {
+                    1
+                } else {
+                    proposed - (ARITY - 1)
+                }
+            };
+            let array_len: u32 = (segment_count + ARITY - 1) * segment_length;
+            (array_len as usize, segment_count)
+        };
+        let segment_count_length = segment_count * segment_length;
+
+        let mut alone = vec![0u32; num_fingerprints];
+        let mut t2count = vec![0u8; num_fingerprints];
+        let mut t2hash = vec![0u64; num_fingerprints];
+        let mut reverse_h = vec![0u8; db_size];
+        let mut reverse_order = vec![0u64; db_size + 1];
+        reverse_order[db_size] = 1;
+
+        let mut hash_to_key = HashMap::new();
+
+        let block_bits = {
+            let mut block_bits = 1;
+            while (1 << block_bits) < segment_count {
+                block_bits += 1;
+            }
+            block_bits
+        };
+        let block_bits_mask = (1 << block_bits) - 1;
+
+        let start_pos_len: usize = 1 << block_bits;
+        let mut start_pos = vec![0usize; start_pos_len];
+
+        let mut h012 = [0u32; 5];
+
+        let mut done = false;
+        let mut ultimate_size = 0;
+
+        let mut seed = [0u8; 32];
+        let mut rng = ChaCha20Rng::from_entropy();
+
+        for _ in 0..max_attempt_count {
+            rng.fill_bytes(&mut seed);
+
+            for i in 0..start_pos_len {
+                start_pos[i] = (((i as u64) * (db_size as u64)) >> block_bits) as usize;
+            }
+
+            for &key in db.keys() {
+                let hashed_key = hash_of_key(key);
+                let hash = mix256(&hashed_key, &seed);
+
+                let mut segment_index = hash >> (64 - block_bits);
+                while reverse_order[start_pos[segment_index as usize] as usize] != 0 {
+                    segment_index += 1;
+                    segment_index &= block_bits_mask;
+                }
+
+                reverse_order[start_pos[segment_index as usize] as usize] = hash;
+                start_pos[segment_index as usize] += 1;
+
+                hash_to_key.insert(hash, key);
+            }
+
+            let mut error = false;
+            for i in 0..db_size {
+                let hash = reverse_order[i];
+
+                let (h0, h1, h2) = hash_batch(hash, segment_length, segment_count_length);
+
+                let (h0, h1, h2) = (h0 as usize, h1 as usize, h2 as usize);
+
+                t2count[h0] += 4;
+                t2hash[h0] ^= hash;
+
+                t2count[h1] += 4;
+                t2count[h1] ^= 1;
+                t2hash[h1] ^= hash;
+
+                t2count[h2] += 4;
+                t2count[h2] ^= 2;
+                t2hash[h2] ^= hash;
+
+                error = t2count[h0] < 4 || t2count[h1] < 4 || t2count[h2] < 4;
+            }
+
+            if error {
+                reverse_order[..db_size].fill(0);
+                t2count.fill(0);
+                t2hash.fill(0);
+
+                continue;
+            }
+
+            let mut qsize = 0;
+            for i in 0..num_fingerprints {
+                alone[qsize] = i as u32;
+                if (t2count[i] >> 2) == 1 {
+                    qsize += 1;
+                }
+            }
+
+            let mut stack_size = 0;
+            while qsize > 0 {
+                qsize -= 1;
+
+                let index = alone[qsize] as usize;
+                if (t2count[index] >> 2) == 1 {
+                    let hash = t2hash[index];
+                    let found: u8 = t2count[index] & 3;
+
+                    reverse_h[stack_size] = found;
+                    reverse_order[stack_size] = hash;
+                    stack_size += 1;
+
+                    let (h0, h1, h2) = hash_batch(hash, segment_length, segment_count_length);
+
+                    h012[1] = h1;
+                    h012[2] = h2;
+                    h012[3] = h0;
+                    h012[4] = h012[1];
+
+                    let other_index1 = h012[(found + 1) as usize] as usize;
+                    alone[qsize] = other_index1 as u32;
+                    if (t2count[other_index1] >> 2) == 2 {
+                        qsize += 1;
+                    }
+
+                    t2count[other_index1] -= 4;
+                    t2count[other_index1] ^= mod3(found + 1);
+                    t2hash[other_index1] ^= hash;
+
+                    let other_index2 = h012[(found + 2) as usize] as usize;
+                    alone[qsize] = other_index2 as u32;
+                    if (t2count[other_index2] >> 2) == 2 {
+                        qsize += 1;
+                    }
+
+                    t2count[other_index2] -= 4;
+                    t2count[other_index2] ^= mod3(found + 2);
+                    t2hash[other_index2] ^= hash;
+                }
+            }
+
+            if stack_size == db_size {
+                ultimate_size = stack_size;
+                done = true;
+
+                break;
+            }
+
+            reverse_order[..db_size].fill(0);
+            t2count.fill(0);
+            t2hash.fill(0);
+        }
+
+        if !done {
+            return None;
+        }
+
+        Some((
+            BinaryFuseFilter {
+                seed,
+                arity: ARITY,
+                segment_length,
+                segment_count_length,
+                num_fingerprints,
+                filter_size: ultimate_size,
+                mat_elem_bit_len,
+            },
+            reverse_order,
+            reverse_h,
+            hash_to_key,
+        ))
+    }
+
+    pub fn construct_4_wise_xor_filter<'a>(
+        db: &HashMap<&'a [u8], &[u8]>,
+        mat_elem_bit_len: usize,
+        max_attempt_count: usize,
+    ) -> Option<(BinaryFuseFilter, Vec<u64>, Vec<u8>, HashMap<u64, &'a [u8]>)> {
+        const ARITY: u32 = 4;
+
         let db_size = db.len();
         if db_size == 0 {
             return None;
