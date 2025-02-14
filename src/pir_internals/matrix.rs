305184610030e17561cb3048b3ecac630d8739c0
Anjan Roy <anjan.roy@tii.ae>
2025-02-14 11:32:18 +0400

    Improve generation of matrix from seed bytes
    Signed-off-by: Anjan Roy <anjan.roy@tii.ae>

--- src/pir_internals/matrix.rs
+++ src/pir_internals/matrix.rs
@@ -175,40 +175,20 @@ impl Matrix {
     pub fn generate_from_seed(rows: usize, cols: usize, seed: &[u8; SEED_BYTE_LEN]) -> Result<Matrix, ChalametPIRError> {
         let mut hasher = Shake128::default();
         hasher.update(seed);
-
         let mut reader = hasher.finalize_xof();
 
-        let mut buffer = [0u8; 168];
-        reader.read(&mut buffer);
+        let mut elems = vec![0u32; rows * cols];
+        let elems_byte_len = elems.len() * std::mem::size_of::<u32>();
 
-        let mut mat = Matrix::new(rows, cols)?;
-        let num_elems = mat.rows * mat.cols;
+        unsafe {
+            let ptr_elems = elems.as_mut_ptr();
+            let ptr_elem_bytes: *mut u8 = ptr_elems.cast();
+            let elem_bytes = core::slice::from_raw_parts_mut(ptr_elem_bytes, elems_byte_len);
 
-        let mut cur_elem_idx = 0;
-        let mut buf_offset = 0;
-
-        while cur_elem_idx < num_elems {
-            let fillable_num_elems_from_buf = (buffer.len() - buf_offset) / 4;
-            if branch_opt_util::unlikely(fillable_num_elems_from_buf == 0) {
-                reader.read(&mut buffer);
-                buf_offset = 0;
-            }
-
-            let required_num_elems = num_elems - cur_elem_idx;
-            let to_be_filled_num_elems = min(fillable_num_elems_from_buf, required_num_elems);
-
-            let mut local_idx = cur_elem_idx;
-            while branch_opt_util::likely(local_idx < (cur_elem_idx + to_be_filled_num_elems)) {
-                mat.elems[local_idx] = u32::from_le_bytes(buffer[buf_offset..(buf_offset + 4)].try_into().unwrap());
-
-                local_idx += 1;
-                buf_offset += std::mem::size_of::<u32>();
-            }
-
-            cur_elem_idx += to_be_filled_num_elems;
+            reader.read(elem_bytes);
         }
 
-        Ok(mat)
+        Matrix::from_values(rows, cols, elems)
     }
 
     /// Generates a row/ column vector with the given dimensions, where each element is sampled from a uniform ternary distribution {0, 1, -1}.
